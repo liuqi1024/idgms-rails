e28a233443201e6549d640c51e7ce277c9d5be5b
liuqi1024 <keyboarder5211@gmail.com>
2014-04-16 11:35:20 +0800

    db structure

---
+++ db/structure.sql
@@ -0,0 +1,241 @@
+CREATE TABLE `game_groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `game_id` int(11) DEFAULT NULL,
+  `game_program_id` int(11) DEFAULT NULL,
+  `code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `torrent` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `serial_no` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_game_groups_on_game_id` (`game_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `game_programs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `game_id` int(11) DEFAULT NULL,
+  `version` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_game_programs_on_game_id` (`game_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `games` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `par_value` int(11) DEFAULT NULL,
+  `sub_gamesize` int(11) DEFAULT NULL,
+  `pool_size` int(11) DEFAULT NULL,
+  `pack_size` int(11) DEFAULT NULL,
+  `packs_per_pool` int(11) DEFAULT NULL,
+  `packs_per_carton` int(11) DEFAULT NULL,
+  `first_ticket_no` int(11) DEFAULT NULL,
+  `first_pack_no` int(11) DEFAULT NULL,
+  `first_pool_no` int(11) DEFAULT NULL,
+  `poolcount_per_printunit` int(11) DEFAULT NULL,
+  `random_no` int(11) DEFAULT NULL,
+  `current_pack_no` int(11) DEFAULT NULL,
+  `version` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `state` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `completed_at` datetime DEFAULT NULL,
+  `desc` text COLLATE utf8_unicode_ci,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `operations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `desc` text COLLATE utf8_unicode_ci,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `patches` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `product_batch_id` int(11) DEFAULT NULL,
+  `ticket_count` int(11) DEFAULT NULL,
+  `pack_no_totals` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `progress` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `progress_test` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `state` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_patches_on_product_batch_id` (`product_batch_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `product_batches` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `game_id` int(11) DEFAULT NULL,
+  `worksheet_id` int(11) DEFAULT NULL,
+  `code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nation_code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `state` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `progress` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `progress_test` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `progress_sample` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ticket_count` int(11) DEFAULT NULL,
+  `print_unit_count` int(11) DEFAULT NULL,
+  `pool_count` int(11) DEFAULT NULL,
+  `subgame_count` int(11) DEFAULT NULL,
+  `start_pack_no` int(11) DEFAULT NULL,
+  `start_pool_no` int(11) DEFAULT NULL,
+  `start_subgame_no` int(11) DEFAULT NULL,
+  `output_pool_no_from` int(11) DEFAULT NULL,
+  `output_pool_no_to` int(11) DEFAULT NULL,
+  `print_unit_totals` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `print_unit_completes` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `completed_at` datetime DEFAULT NULL,
+  `desc` text COLLATE utf8_unicode_ci,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_product_batches_on_worksheet_id` (`worksheet_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `product_units` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `game_id` int(11) DEFAULT NULL,
+  `product_batch_id` int(11) DEFAULT NULL,
+  `code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_product_units_on_game_id` (`game_id`),
+  KEY `index_product_units_on_product_batch_id` (`product_batch_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `role_operations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) DEFAULT NULL,
+  `operation_id` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `resource_id` int(11) DEFAULT NULL,
+  `resource_type` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_roles_on_name` (`name`),
+  KEY `index_roles_on_name_and_resource_type_and_resource_id` (`name`,`resource_type`,`resource_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  UNIQUE KEY `unique_schema_migrations` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `tickets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ticket_no` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `game_id` int(11) DEFAULT NULL,
+  `game_group_id` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_tickets_on_game_id` (`game_id`),
+  KEY `index_tickets_on_game_group_id` (`game_group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `transport_batches` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_batch_id` int(11) DEFAULT NULL,
+  `code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `start_pool_no` int(11) DEFAULT NULL,
+  `end_pool_no` int(11) DEFAULT NULL,
+  `print_unit_completes` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `state` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_transport_batches_on_product_batch_id` (`product_batch_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `encrypted_password` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `reset_password_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `reset_password_sent_at` datetime DEFAULT NULL,
+  `remember_created_at` datetime DEFAULT NULL,
+  `sign_in_count` int(11) NOT NULL DEFAULT '0',
+  `current_sign_in_at` datetime DEFAULT NULL,
+  `last_sign_in_at` datetime DEFAULT NULL,
+  `current_sign_in_ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last_sign_in_ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `loginname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_users_on_email` (`email`),
+  UNIQUE KEY `index_users_on_reset_password_token` (`reset_password_token`),
+  UNIQUE KEY `index_users_on_loginname` (`loginname`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `users_roles` (
+  `user_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  KEY `index_users_roles_on_user_id_and_role_id` (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `worksheets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `game_id` int(11) DEFAULT NULL,
+  `print_unit_count` int(11) DEFAULT NULL,
+  `ticket_count` int(11) DEFAULT NULL,
+  `code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `printbatch` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `version` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `generate_at` datetime DEFAULT NULL,
+  `completed_at` datetime DEFAULT NULL,
+  `state` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `desc` text COLLATE utf8_unicode_ci,
+  `current_pack_no` int(11) DEFAULT NULL,
+  `current_pool_no` int(11) DEFAULT NULL,
+  `current_subgame_no` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_worksheets_on_game_id` (`game_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO schema_migrations (version) VALUES ('20140328053100');
+
+INSERT INTO schema_migrations (version) VALUES ('20140328064822');
+
+INSERT INTO schema_migrations (version) VALUES ('20140328065837');
+
+INSERT INTO schema_migrations (version) VALUES ('20140328072341');
+
+INSERT INTO schema_migrations (version) VALUES ('20140328075352');
+
+INSERT INTO schema_migrations (version) VALUES ('20140328080259');
+
+INSERT INTO schema_migrations (version) VALUES ('20140328080304');
+
+INSERT INTO schema_migrations (version) VALUES ('20140328090853');
+
+INSERT INTO schema_migrations (version) VALUES ('20140331083512');
+
+INSERT INTO schema_migrations (version) VALUES ('20140401014657');
+
+INSERT INTO schema_migrations (version) VALUES ('20140401024116');
+
+INSERT INTO schema_migrations (version) VALUES ('20140402025908');
+
+INSERT INTO schema_migrations (version) VALUES ('20140402025944');
+
+INSERT INTO schema_migrations (version) VALUES ('20140414073346');
